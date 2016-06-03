96243ec4924c0726ac61b03bc0053de6296ae19d
Rafael Passarela <rafaelpassarela@gmail.com>
2016-06-03 15:09:31 -0300

    Detect when the session is lock/unlock

--- Program.cs
+++ Program.cs
@@ -5,20 +5,46 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Win32;
 
+
 namespace LockForMe
 {
     class Program
     {
         private static SessionController _session;
+        private static bool isLock;
         static void Main(string[] args)
         {
             _session = new SessionController();
+            SystemEvents.SessionSwitch += new SessionSwitchEventHandler(SystemEvents_SessionSwitch);
 
-            Console.WriteLine("Esperando...");
+            Console.WriteLine("The Session Switch is being monitored now.");
+            Console.WriteLine(" - To close, type CLOSE and hit Return.");
+            Console.WriteLine(" - To change the Idle time, type TIME 60, when 60 is the seconds to lock the       session for you.");
+            Console.WriteLine(" ");
 
-            System.Threading.Thread.Sleep(5000);
+            var cmd = "";
+            while (!cmd.ToUpper().Equals("CLOSE"))
+            {
+                cmd = Console.ReadLine();
+            }
+        }
 
-            Console.Write(_session.IsSessionLock ? "Session Lock" : "Session Unlock");
+        static void SystemEvents_SessionSwitch(object sender, Microsoft.Win32.SessionSwitchEventArgs e)
+        {
+            switch (e.Reason)
+            {
+                case SessionSwitchReason.SessionLock:
+                    //I left my desk
+                    isLock = true;
+                    Console.WriteLine("I left my desk");
+                    break;
+                case SessionSwitchReason.SessionUnlock:
+                case SessionSwitchReason.SessionLogon:
+                    //I returned to my desk
+                    isLock = false;
+                    Console.WriteLine("I returned to my desk");
+                    break;
+            }
         }
     }
 }
