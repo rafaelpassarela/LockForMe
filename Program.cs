a6f19a0f5a2e15c8b14aa8cf403e68180df69312
Rafael Passarela <rafaelpassarela@gmail.com>
2016-06-03 13:39:27 -0300

    Project started

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LockForMe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("It's Ok :D");
+        }
+    }
+}
